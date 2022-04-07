9bb53ebc865cfb254460d9ca5aba68cc780c595f
User <User@WIN-7RV6M1VCLCV>
2022-04-07 22:29:23 +0300

    Добавьте файлы проекта.

---
+++ ЛР4_Мак_Мин/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace ЛР4_Мак_Мин
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(500, 42);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(213, 78);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(500, 180);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(186, 34);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(555, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Изначальный массив";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(116, 180);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(273, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "минимальный элемент";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(116, 273);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(273, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "максимальный элемент";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(500, 262);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(186, 34);
+            this.textBox3.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(344, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "введите массив";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ЛР4_Мак_Мин/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ЛР4_Мак_Мин
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+  //обрработчик ошибок try -catch для предотвращения не правильного ввода ошибок пользователя
+  //сначала выполняется блок try, если возникает ошибка, то срабатывет блок catch
+            try
+            {
+                string username = textBox1.Text;//получаем строку из текстового поля , типа  данных string
+                int[] ia = username.Split(' ').Select(n => Convert.ToInt32(n)).ToArray();//делим ее посимвольно если стретился пробел
+
+                int minValue = ia.Min();//находим минимальное число в массиве с помощью встроенной функции Min()
+
+                textBox2.Text = ("Минимальное число: " + minValue);//выводим это число
+            }
+            catch (Exception ex) //блок обработки ошибки
+            {
+                MessageBox.Show("Введите массив через пробел"+ex); //вывод сообщения об ошибке
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //обрработчик ошибок try -catch для предотвращения не правильного ввода ошибок пользователя
+            //сначала выполняется блок try, если возникает ошибка, то срабатывет блок catch
+            try
+            {
+                string username = textBox1.Text;//объявление переменной username типа стринг и присвоение значения из текстбокса1
+                //объявление одномерного массива типа int.
+                //запись элементов в массив происходит по элементно путем деления строки username на символы,
+                //если встречается пробел (условие деления прописывается в ковычках в скобках)
+                int[] ia = username.Split(' ').Select(n => Convert.ToInt32(n)).ToArray();
+
+                int maxValue = ia.Max();//находим максимальное число (встроенной функцией Мах)
+
+                textBox3.Text = ("Максимальое число: " + maxValue); //вывод найденного значения в тексбок3
+            }
+            catch (Exception ex) //блок обработки ошибки
+            {
+                MessageBox.Show("Введите массив через пробел" + ex); //вывод сообщения об ошибке
+            }
+
+
+        }
+    }
+}
